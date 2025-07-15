def40a89f188f1ba52973161d131862e6960562c
pnilssson <p.nilsson95@hotmail.com>
2025-07-15 21:27:27 +0200

    Loop over work places.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,3 +3,10 @@ export interface Project {
   description: string;
   url: string;
 };
+
+export interface WorkItem {
+  company: string;
+  period: string;
+  description: string;
+  tech: string[];
+};
